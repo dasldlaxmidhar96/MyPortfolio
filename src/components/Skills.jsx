e2d991d0a4a4d9da3355f958c1f3472fe6a37145
LAXMIDHAR DAS <laxmidhardas0000@gmail.com>
2023-04-13 13:25:16 +0530

    new

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,7 +19,7 @@ const Skills = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
-          <p className="py-4 text-xl">
+          <p className="py-4 text-l">
             These are the technologies I've worked with
           </p>
         </div>
